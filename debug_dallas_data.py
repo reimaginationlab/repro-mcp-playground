c42c73a3b14964d009f01ebacf64d77b71563dfb
Quinn McHenry <quinn.mchenry@smallplanet.com>
2025-10-23 11:54:20 -0400

    Add Dallas debug data

---
+++ debug_dallas_data.py
@@ -0,0 +1,208 @@
+"""
+Static data for DEBUG_DALLAS mode
+Replace the placeholder data below with your actual static content
+"""
+
+# Static policy data for Texas (Dallas)
+POLICY_DATA = {
+    "state_id": "texas",
+    "state_name": "Texas",
+    "policies": {
+        "abortion_bans": {
+            "abortion_ban": "Total Ban",
+            "ban_exceptions": ["PLACEHOLDER - Add ban exceptions here"],
+        },
+        "Minors": {
+            "PLACEHOLDER": "Add minors policy data here"
+        },
+        "TMAB": {
+            "tmab": ["PLACEHOLDER - Add TMAB status here"]
+        }
+    }
+}
+
+# Static clinic data for Dallas area
+CLINIC_DATA = {
+   "clinics" : [
+      {
+         "addressLink" : "https://www.google.com/maps/search/?api=1&query=2226%20E%20Central%2CWichita%2CKS%2C67214",
+         "appointmentForm" : "https://docasap.com/white-label/key_prac_id/2876/key_map/1/key_level/4/key_type/LINK/hide_filter/1/hide_header/1/hide_footer/1/hide_other_provider/1/key_partner_code/PPFA/external_src/1/hide_profile/1/hide_insurance/1/hide_location/1/hide_profile_infoset/1/key_language/",
+         "canWaiveWaitingPeriod" : True,
+         "city" : "Wichita",
+         "dateupdated" : "2025-09-17",
+         "distance" : 337,
+         "driveTime" : 20509.55,
+         "formattedCityState" : "Wichita, Kansas",
+         "hours" : {
+            "Friday" : "8:30 am - 6:00 pm",
+            "Monday" : "8:30 am - 6:00 pm",
+            "Saturday" : "8:30 am - 6:00 pm",
+            "Thursday" : "8:30 am - 6:00 pm",
+            "Tuesday" : "8:30 am - 6:00 pm",
+            "Wednesday" : "10:00 am - 6:00 pm",
+            "source" : "google",
+            "timeZone" : "Central Daylight Time"
+         },
+         "id" : "reccyVnsb2I6k2s52",
+         "latitude" : 37.687692,
+         "longitude" : -97.339044,
+         "maxCostPills" : 775,
+         "naf" : True,
+         "name" : "Planned Parenthood, Wichita Health Center",
+         "phone" : "(913) 345-1400",
+         "services" : {
+            "additionalinfo" : "In-clinic and medication abortion services are available by appointment only.  ",
+            "medLmp" : 12,
+            "overallLmp" : 13.6,
+            "surgical" : True,
+            "surgicalLmp" : 13.6,
+            "translation" : "Spanish, Interpretation by telephone available for other languages"
+         },
+         "state" : "KS",
+         "status" : "OPEN",
+         "streetAddress" : "2226 E Central",
+         "teleServiceStates" : [],
+         "url" : "/clinic/planned-parenthood-wichita-health-center-recXubMccemps1tyw",
+         "urlSlug" : "planned-parenthood-wichita-health-center-recXubMccemps1tyw",
+         "website" : "https://www.plannedparenthood.org/health-center/kansas/wichita/67214/wichita-health-center-2876-90741",
+         "yelp" : "https://www.yelp.com/biz/planned-parenthood-wichita?osq=planned+parenthood",
+         "zipcode" : "67214"
+      },
+      {
+         "acn" : True,
+         "addressLink" : "https://www.google.com/maps/search/?api=1&query=5107%20E.%20Kellogg%20Drive%2CWichita%2CKS%2C67218",
+         "canWaiveWaitingPeriod" : True,
+         "city" : "Wichita",
+         "dateupdated" : "2025-09-17",
+         "distance" : 336.2,
+         "driveTime" : 20516.56,
+         "formattedCityState" : "Wichita, Kansas",
+         "fundingAssistance" : [
+            "NAF in-house"
+         ],
+         "hours" : {
+            "Friday" : "8:00 am - 5:00 pm",
+            "Monday" : "8:00 am - 5:00 pm",
+            "Thursday" : "8:00 am - 5:00 pm",
+            "Tuesday" : "8:00 am - 5:00 pm",
+            "Wednesday" : "8:00 am - 5:00 pm",
+            "source" : "google",
+            "timeZone" : "Central Daylight Time"
+         },
+         "id" : "recvpuq4CxzE5NBXD",
+         "latitude" : 37.678631,
+         "longitude" : -97.2768724,
+         "maxCostPills" : 750,
+         "naf" : True,
+         "name" : "Trust Women Wichita Clinic",
+         "phone" : "(316) 260-6934",
+         "pricelist" : "https://trustwomen.org/services/abortion/",
+         "services" : {
+            "additionalinfo" : "Appointments must be scheduled by phone. \n \nAfter 18.0 weeks, a 2-day procedure is required. Call the Wichita clinic directly for more information on post-18.0-week procedures.\n\nYou may encounter protesters outside near the gated entrance. It is suggested you just drive past them. You do not need to stop at the gates for any reason.",
+            "earliest" : 5,
+            "medLmp" : 11.6,
+            "overallLmp" : 21.6,
+            "surgical" : True,
+            "surgicalLmp" : 21.6,
+            "translation" : "Interpretation by telephone available for other languages, Spanish"
+         },
+         "state" : "KS",
+         "status" : "OPEN",
+         "streetAddress" : "5107 E. Kellogg Drive",
+         "teleServiceStates" : [],
+         "url" : "/clinic/trust-women-wichita-clinic-recrEvsKa8kFr2pgo",
+         "urlSlug" : "trust-women-wichita-clinic-recrEvsKa8kFr2pgo",
+         "website" : "https://trustwomen.org/",
+         "zipcode" : "67218"
+      },
+      {
+         "acn" : True,
+         "addressLink" : "https://www.google.com/maps/search/?api=1&query=555%20N%20Woodlawn%20Blvd%2C%20Building%201%2C%20Suite%20102%2CWichita%2CKS%2C67208",
+         "appointmentForm" : "https://www.ariamedical.org/intake",
+         "canWaiveWaitingPeriod" : True,
+         "city" : "Wichita",
+         "dateupdated" : "2025-09-17",
+         "distance" : 337.3,
+         "driveTime" : 20648.66,
+         "formattedCityState" : "Wichita, Kansas",
+         "fundingAssistance" : [
+            "NAF in-house",
+            "\"pay what you can\" sliding scale"
+         ],
+         "hours" : {
+            "Friday" : "9:00 am - 5:00 pm",
+            "Monday" : "9:00 am - 5:00 pm",
+            "Saturday" : "9:00 am - 5:00 pm",
+            "Sunday" : "9:00 am - 5:00 pm",
+            "Thursday" : "9:00 am - 5:00 pm",
+            "Tuesday" : "9:00 am - 5:00 pm",
+            "Wednesday" : "9:00 am - 5:00 pm",
+            "source" : "google",
+            "timeZone" : "Central Daylight Time"
+         },
+         "id" : "recvCdhYDCskDpdNx",
+         "latitude" : 37.69611,
+         "longitude" : -97.26642,
+         "maxCostPills" : 490,
+         "naf" : True,
+         "name" : "Aria Medical",
+         "phone" : "(316) 350-7688",
+         "services" : {
+            "additionalinfo" : "Appointments are available by appointment only Fridays-Sundays and some weekdays. [Schedule online only](https://www.ariamedical.org/schedule), appointments will not be scheduled by phone or email. \n\nFor any questions, call or text 316 350 7688 Mondays - Sundays. Leave a message if no answer, and the call will be returned.\n\nAria can provide abortions with one trip to the clinic. Appointment length is usually 1-2 hours. ",
+            "earliest" : 4,
+            "medLmp" : 12,
+            "overallLmp" : 12,
+            "translation" : "Interpretation by telephone available for other languages"
+         },
+         "state" : "KS",
+         "status" : "OPEN",
+         "streetAddress" : "555 N Woodlawn Blvd, Building 1, Suite 102",
+         "teleServiceStates" : [],
+         "url" : "/clinic/aria-medical-recBlKspc4Jh4ISjW",
+         "urlSlug" : "aria-medical-recBlKspc4Jh4ISjW",
+         "website" : "https://www.ariamedical.org/",
+         "zipcode" : "67208"
+      },
+      {
+         "addressLink" : "https://www.google.com/maps/search/?api=1&query=2312%20South%20Tucker%20Terrace%2CPittsburg%2CKS%2C66762",
+         "appointmentForm" : "https://docasap.com/provider-profile/center/261931/-1/0/0/0/PPFA/1037965980/0/0/0/0/1?_gl=1*aeugmf*_ga*ODk2NzM0MjI0LjE3MDE1NDI1MzY.*_ga_ENXC8KHJL8*MTc0Mzg4OTE1NC4xNDEuMC4xNzQzODg5MTU2LjAuMC45NjE5NDA3NDI.",
+         "city" : "Pittsburg",
+         "dateupdated" : "2025-09-17",
+         "distance" : 340.5,
+         "driveTime" : 25088.71,
+         "formattedCityState" : "Pittsburg, Kansas",
+         "hours" : {
+            "Friday" : "8:30 am - 6:00 pm",
+            "Monday" : "8:30 am - 6:00 pm",
+            "Saturday" : "8:30 am - 6:00 pm",
+            "Thursday" : "8:30 am - 6:00 pm",
+            "Tuesday" : "8:30 am - 6:00 pm",
+            "Wednesday" : "10:00 am - 6:00 pm",
+            "source" : "google",
+            "timeZone" : "Central Daylight Time"
+         },
+         "id" : "receeJ99C9dFlLmmA",
+         "latitude" : 37.3835641395407,
+         "longitude" : -94.6899031410579,
+         "maxCostPills" : 775,
+         "name" : "Planned Parenthood, Pittsburg Health Center",
+         "phone" : "(913) 345-1400",
+         "services" : {
+            "additionalinfo" : "Abortion services are available by appointment only.",
+            "medLmp" : 12,
+            "overallLmp" : 13.6,
+            "surgical" : True,
+            "surgicalLmp" : 13.6,
+            "translation" : "Interpretation by telephone available for other languages"
+         },
+         "state" : "KS",
+         "status" : "OPEN",
+         "streetAddress" : "2312 South Tucker Terrace",
+         "teleServiceStates" : [],
+         "url" : "/clinic/planned-parenthood-pittsburg-health-center-recRA6YPg0hANrN9v",
+         "urlSlug" : "planned-parenthood-pittsburg-health-center-recRA6YPg0hANrN9v",
+         "website" : "https://www.plannedparenthood.org/health-center/kansas/pittsburg/66762/pittsburg-health-center-90740-90740",
+         "zipcode" : "66762"
+      }
+    ]
+}
